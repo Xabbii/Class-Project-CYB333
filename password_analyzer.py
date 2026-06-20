7377a017bb42280bab5c001dbc62e2ebd60196b6
Javier De La Cruz Osuna <288481341+Xabbii@users.noreply.github.com>
2026-06-19 19:32:44 -0700

    Add score and suggestions variables
    Added initial implementation for password strength analysis.

--- password_analyzer.py
+++ password_analyzer.py
@@ -1,1 +1,22 @@
+# Password Strength Analyzer 
+# Created by Javier De La Cruz for CYB333
+
+# Ask the user to enter a password that will be checked
+password = input("Enter a sample password to check: ")
+
+# Count how many characters are in the password
+password_length = len(password)
+
+# We have to create a score and suggestions list to let the user know if their password is weak, medium, or strong
+score = 0
+suggestions = []
+
+
+# This will display the password check result to the user
+print("Password Received.")
+print("Password length:", password_length)
+
+
+
+
 
