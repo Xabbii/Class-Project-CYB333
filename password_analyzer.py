31ff5579bf055e045b6b54e89865ccc40f49a5a6
Javier De La Cruz Osuna <288481341+Xabbii@users.noreply.github.com>
2026-06-20 01:37:24 -0700

    moved and spaced the script
    Refactor password strength analyzer to improve checks and output.

--- password_analyzer.py
+++ password_analyzer.py
@@ -1,17 +1,31 @@
 # Password Strength Analyzer 
 # Created by Javier De La Cruz for CYB333
 
+
 # Ask the user to enter a password that will be checked
+
 password = input("Enter a sample password to check: ")
 
+
 # Count how many characters are in the password
+
 password_length = len(password)
 
+
+# This will display password received and length result to the user
+
+print("Password Received.")
+print("Password Length:", password_length)
+
+
 # We have to create a score and suggestions list to let the user know if their password is weak, medium, or strong
+
 score = 0
 suggestions = []
 
+
 # This will check if the password meets the minimum length requirements
+
 if password_length >= 8:
   score = score + 1
   print("Length---> Satisfied")
@@ -19,7 +33,9 @@ else:
   suggestions.append("Use at least 8 characters.")
   print("Length---> Not Satisfied")
 
+
 # This will check if the password has any uppercases
+
 has_uppercase = False
 
 for letter in password:
@@ -33,7 +49,9 @@ else:
   suggestions.append("Add an uppercase letter.")
   print("Uppercase---> Not Satisfied")
 
+
 # This will check if any words in the password has any lowercase letters
+
 has_lowercase = False
 
 for letter in password:
@@ -47,7 +65,9 @@ else:
   suggestions.append("Add a lowercase letter.")
   print("Lowercase---> Not Satisfied")
 
+
 # In this section we will see if the password has any numbers
+
 has_numbers = False
 
 for numbers in password:
@@ -61,7 +81,9 @@ else:
   suggestions.append("Use at least one number.")
   print("Numbers---> Not Satisfied")
 
+
 # Here it will check if there is any special characters
+
 has_special = False
 
 for special in password:
@@ -74,20 +96,17 @@ if has_special:
 else:
   suggestions.append("Use at least one special character.")
   print("Special characters---> Not Satisfied")
-
-# This will display the password check result to the user
-print("Password Received.")
-print("Password length:", password_length)
+  
 
 # Here will display the password strength depending on score
+
 if score == 5:
   print("Password Strength: Strong")
-
 elif score >= 3:
   print("Password Strength: Medium")
-
 else:
   print("Password Strength: Weak")
+
 
 # here will show the suggestions if the password is missing requirements
 
